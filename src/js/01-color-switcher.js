c66133470a6180f99e7945e9800c60c37aee2bf9
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-06-16 13:49:53 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onstartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+const colorPicker = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const hexColor = getRandomHexColor();
+
+      refs.body.style.backgroundColor = hexColor;
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+function onstartBtnClick(evn) {
+  colorPicker.start();
+}
+
+function onStopBtnClick(evn) {
+  colorPicker.stop();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
